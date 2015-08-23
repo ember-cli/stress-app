1fafafb8a1668d33d4ae98c8a5e9eac865662120
Stefan Penner <stefan.penner@gmail.com>
2015-08-23 15:03:08 -0700

    ensure we actually hit the pathological scenario ember-cli-jquery-ui hit

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    'ember-cli-jquery-ui': {
+      theme: 'ui-darkness'
+    },
     sourcemaps: {
       enabled: false,
       sourceMap: false
